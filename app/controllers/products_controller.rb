6e16f201b0419586c5df835e4ebf7c82f027bb2e
Erick Solar <ericksolarb@gmail.com>
2017-08-10 15:47:53 -0400

    Crea la ruta /products/:id/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,14 @@ class ProductsController < ApplicationController
 		end
 	end
 
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def edit
+		@product = Product.find(params[:id])		
+	end
+
 	private
 
 		def product_params
